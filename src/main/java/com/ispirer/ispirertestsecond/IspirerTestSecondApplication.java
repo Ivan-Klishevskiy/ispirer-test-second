3df23d477766ead1f33dc389e087a66f2687d030
KLISHEVSKIY\Kliis <i.klishevskiy@mail.ru>
2022-02-13 20:19:24 +0300

    Completed task.

---
+++ src/main/java/com/ispirer/ispirertestsecond/IspirerTestSecondApplication.java
@@ -0,0 +1,13 @@
+package com.ispirer.ispirertestsecond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IspirerTestSecondApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IspirerTestSecondApplication.class, args);
+    }
+
+}
